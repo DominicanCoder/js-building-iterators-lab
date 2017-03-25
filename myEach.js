455b8f8ff4981adce37dd9b57a6e41e64af81a0b
DominicanCoder <rflores.rsj@gmail.com>
2017-03-25 16:58:50 -0600

    we modify many function

--- myEach.js
+++ myEach.js
@@ -1,13 +1,21 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/ForEach
 
-function myEach(Array, callback) {
-  
-var orange = ["Gold", "mandarina"];
-for(var i=0; i < orange.length; i++){
-    console.log(orange[i] + "!");
+console.log("running!");
+
+function myEach(arrayPassedIn, functionPassedIn) {
+
+	//use the function that was passed in on each item in the
+	//  array.
+	for(var i=0; i < arrayPassedIn.length; i++){
+		functionPassedIn(arrayPassedIn[i], i, arrayPassedIn);
+	}
 }
 
-}
+var myExampleArray = ["a", "b", "c", "d"];
+
+myEach(myExampleArray, function printer(currentValueInArray, currentIndex) {
+	console.log(currentValueInArray + " " + currentIndex);
+});
 
 /*
  Best if you don't code out here.
--- myMap.js
+++ myMap.js
@@ -2,10 +2,32 @@
 
 function myMap(arr, callback) {
 
-//  CODE INSIDE HERE   //
+	//create a new array, empty!
+	var cup = [];
+
+
+	//for loop
+	for(var i=0; i < arr.length; i++){
+		//invoke the function they give with the current array item
+		//get the value that the function gives us
+		var currentReturnValue = callback(arr[i],i);
+		//console.log("this is the current value: " + currentReturnValue);
+		//put value in the new array
+		cup.push(currentReturnValue);
+	}
+	//return the new array	
+return cup;
 
 }
 
+
+var myExampleArray = ["a", "b", "c", "d"];
+
+var newarray = myMap(myExampleArray, function printer(currentValueInArray, currentIndex) {
+	return currentValueInArray + " !!!!! " + currentIndex;
+	//console.log(currentValueInArray + " !!!!! " + currentIndex);
+});
+console.log(newarray);
 /*
  Best if you don't code out here.
  If you want to check your code, use `index.js`!
